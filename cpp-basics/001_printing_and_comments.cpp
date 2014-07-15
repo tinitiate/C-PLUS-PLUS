c7ab3133e49048b42d315993bf835ffef21b6db7
Venkata Bhattaram <tinitiate@users.noreply.github.com>
2014-07-15 13:45:13 +0530

    Create 001_printing_and_comments.cpp

---
+++ cpp-basics/001_printing_and_comments.cpp
@@ -0,0 +1,52 @@
+// =================================================================================
+// TOPIC: C++  Comments and Printing to Console
+// =================================================================================
+// NOTES : * Comments in C++, Singleline and Multiline comments
+//         * Simple Printing a text in C++
+//         * cout <<"SOME STRING<<;  prints a String
+//         * putchar("X");           Prints a Char
+// =================================================================================
+//
+// FILE-NAME : 001_printing_and_comments.cpp
+//
+// FILES NEEDED TO RUN THIS PROGRAM
+//           : N/A
+//
+// AUTHOR    : tinitiate.com / Venkata Bhattaram
+//             (c) 2014
+//
+// =================================================================================
+
+
+// This is a single line comment in C++
+
+/*
+this
+is a
+multiline
+comment in C++
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+// The main() is the entry point to the program
+int main()
+{
+
+   // Printing a Char
+   putchar('A');
+   // "cout" Prints in C++
+   cout << "Welcome to Tinitiate C++ tutorials .."; // prints Hello World
+
+   // This will prevent the console from exiting
+   cout << "\n";
+   return 0;
+}
+
+// =================================================================================
+// TAGS: CPP C++ VC++ Comments Printing in C++ VC++ CPP
+// =================================================================================
